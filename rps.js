afce8012f60c07e678266d08307b524482634de5
erich333 <erichodowd@gmail.com>
2022-06-05 07:41:15 -0400

    Modify playRound() to return tie, win or loss

--- rps.js
+++ rps.js
@@ -19,19 +19,19 @@ function playRound(playerSelection, computerSelection) {
         playerSelection.substr(1).toLowerCase();
 
     if(playerSelection === computerSelection) {
-        return "Tie! You both chose " + playerSelection + "!";
+        return "tie";
     } else if(playerSelection === "Rock" && computerSelection === "Paper") {
-        return "You Lose! Paper beats Rock!";
+        return "loss";
     } else if(playerSelection === "Rock" && computerSelection === "Scissors") {
-        return "You Win! Rock beats Scissors!";
+        return "win";
     } else if(playerSelection === "Paper" && computerSelection === "Scissors") {
-        return "You Lose! Scissors beats Paper!";
+        return "loss";
     } else if(playerSelection === "Paper" && computerSelection === "Rock") {
-        return "You Win! Paper beats Rock!";
+        return "win";
     }  else if(playerSelection === "Scissors" && computerSelection === "Rock") {
-        return "You Lose! Rock beats Scissors!";
+        return "loss";
     }  else if(playerSelection === "Scissors" && computerSelection === "Paper") {
-        return "You Win! Scissors beats Paper!";
+        return "win";
     } else {
         console.error("Unexpected game outcome in playRound!");
     }
