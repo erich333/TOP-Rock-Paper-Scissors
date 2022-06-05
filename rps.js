4f3b78104c13a42a4a235bac52892895d57eae32
erich333 <erichodowd@gmail.com>
2022-06-05 07:22:20 -0400

    Add game logic to playRound

--- rps.js
+++ rps.js
@@ -11,15 +11,29 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    if( typeof(playerSelection) != 'string' ) {
+    if(typeof(playerSelection) != "string") {
         console.error("Invalid player selection in playRound");
     }
 
     playerSelection = playerSelection[0].toUpperCase() + 
         playerSelection.substr(1).toLowerCase();
-    
-    console.log(playerSelection);
-    
-    
+
+    if(playerSelection === computerSelection) {
+        return "Tie! You both chose " + playerSelection + "!";
+    } else if(playerSelection === "Rock" && computerSelection === "Paper") {
+        return "You Lose! Paper beats Rock!";
+    } else if(playerSelection === "Rock" && computerSelection === "Scissors") {
+        return "You Win! Rock beats Scissors!";
+    } else if(playerSelection === "Paper" && computerSelection === "Scissors") {
+        return "You Lose! Scissors beats Paper!";
+    } else if(playerSelection === "Paper" && computerSelection === "Rock") {
+        return "You Win! Paper beats Rock!";
+    }  else if(playerSelection === "Scissors" && computerSelection === "Rock") {
+        return "You Lose! Rock beats Scissors!";
+    }  else if(playerSelection === "Scissors" && computerSelection === "Paper") {
+        return "You Win! Scissors beats Paper!";
+    } else {
+        console.error("Unexpected game outcome in playRound!");
+    }
     
 }
